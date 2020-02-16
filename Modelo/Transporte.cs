00255609bd8b0cb17ee50357193899a2f90df21d
Fifajunior1 <57728629+Fifajunior1@users.noreply.github.com>
2020-02-16 12:47:00 -0600

    SF_Inicio de conf de la clase Transporte

--- Modelo/Transporte.cs
+++ Modelo/Transporte.cs
@@ -8,5 +8,11 @@ namespace ERP_ventas.Modelo
 {
     class Transporte
     {
+        public int ID { get; set; }
+        public string Placas { get; set; }
+        public string Marca { get; set; }
+        public int Anio { get; set; }
+        public int Capacidad { get; set; }
+
     }
 }
