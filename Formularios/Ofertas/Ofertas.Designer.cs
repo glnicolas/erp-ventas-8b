cf19d5609185f39d900baa0d0b253bdc10fcfa10
AbrahamBarriga <58277699+AbrahamBarriga@users.noreply.github.com>
2020-03-13 13:13:41 -0600

    Entrega 2.0 _ AB
    se agregó la carpeta de ofertas faltante

---
+++ Formularios/Ofertas/Ofertas.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ERP_ventas.Formularios.Ofertas
+{
+    partial class Ofertas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.dataGridViewClientes = new System.Windows.Forms.DataGridView();
+            this.paginaxdey = new System.Windows.Forms.Label();
+            this.siguienteBtn = new System.Windows.Forms.Button();
+            this.anteriorBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClientes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1018, 74);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clientes Individuales";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Image = global::ERP_ventas.Properties.Resources.eliminar2;
+            this.btnEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminar.Location = new System.Drawing.Point(96, 80);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(77, 38);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar  ";
+            this.btnEliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Image = global::ERP_ventas.Properties.Resources.plus;
+            this.btnNuevo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNuevo.Location = new System.Drawing.Point(12, 80);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(78, 38);
+            this.btnNuevo.TabIndex = 4;
+            this.btnNuevo.Tag = "Agregar nuevo registro";
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewClientes
+            // 
+            this.dataGridViewClientes.AllowUserToAddRows = false;
+            this.dataGridViewClientes.AllowUserToDeleteRows = false;
+            this.dataGridViewClientes.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridViewClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewClientes.Location = new System.Drawing.Point(8, 124);
+            this.dataGridViewClientes.MultiSelect = false;
+            this.dataGridViewClientes.Name = "dataGridViewClientes";
+            this.dataGridViewClientes.ReadOnly = true;
+            this.dataGridViewClientes.RowHeadersVisible = false;
+            this.dataGridViewClientes.RowHeadersWidth = 51;
+            this.dataGridViewClientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewClientes.Size = new System.Drawing.Size(1002, 351);
+            this.dataGridViewClientes.TabIndex = 5;
+            // 
+            // paginaxdey
+            // 
+            this.paginaxdey.AutoSize = true;
+            this.paginaxdey.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paginaxdey.Location = new System.Drawing.Point(462, 523);
+            this.paginaxdey.Name = "paginaxdey";
+            this.paginaxdey.Size = new System.Drawing.Size(59, 20);
+            this.paginaxdey.TabIndex = 29;
+            this.paginaxdey.Text = "1 de 2";
+            // 
+            // siguienteBtn
+            // 
+            this.siguienteBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siguienteBtn.Image = global::ERP_ventas.Properties.Resources.right_squared_32px;
+            this.siguienteBtn.Location = new System.Drawing.Point(561, 513);
+            this.siguienteBtn.Name = "siguienteBtn";
+            this.siguienteBtn.Size = new System.Drawing.Size(30, 43);
+            this.siguienteBtn.TabIndex = 28;
+            this.siguienteBtn.UseVisualStyleBackColor = true;
+            // 
+            // anteriorBtn
+            // 
+            this.anteriorBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.anteriorBtn.Enabled = false;
+            this.anteriorBtn.Image = global::ERP_ventas.Properties.Resources.back_32px;
+            this.anteriorBtn.Location = new System.Drawing.Point(395, 513);
+            this.anteriorBtn.Name = "anteriorBtn";
+            this.anteriorBtn.Size = new System.Drawing.Size(30, 43);
+            this.anteriorBtn.TabIndex = 27;
+            this.anteriorBtn.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 124);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1018, 377);
+            this.dataGridView1.TabIndex = 26;
+            // 
+            // Ofertas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 560);
+            this.Controls.Add(this.paginaxdey);
+            this.Controls.Add(this.siguienteBtn);
+            this.Controls.Add(this.anteriorBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridViewClientes);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.panel1);
+            this.Name = "Ofertas";
+            this.Text = "Ofertas";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewClientes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.DataGridView dataGridViewClientes;
+        private System.Windows.Forms.Label paginaxdey;
+        private System.Windows.Forms.Button siguienteBtn;
+        private System.Windows.Forms.Button anteriorBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Formularios/Ofertas/Ofertas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ERP_ventas.Formularios.Ofertas
+{
+    public partial class Ofertas : Form
+    {
+        public Ofertas()
+        {
+            InitializeComponent();
+        }
+    }
+}
