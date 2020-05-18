585a0dc2d98d26db1773696b7c2d9780f5423fe4
Fifajunior1 <57728629+Fifajunior1@users.noreply.github.com>
2020-05-18 17:33:09 -0500

    Creacion de la interfaz EnviosVentas y su codigo en Modelo

---
+++ Modelo/EnviosVentas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_ventas.Modelo
+{
+    class EnviosVentas
+    {
+        public int idEnvio { set; get; }
+        public int idVenta { set; get; }
+        public DateTime fechaEntregaPlaneada { set; get; }
+        public DateTime fechaEntregaReal { set; get; }
+
+        public EnviosVentas(int idEnvio, int idVenta, DateTime fechaEntregaPlaneada, DateTime fechaEntregaReal) 
+        {
+            this.idEnvio = idEnvio;
+            this.idVenta = idVenta;
+            this.fechaEntregaPlaneada = fechaEntregaPlaneada;
+            this.fechaEntregaReal = fechaEntregaReal;
+        }
+    }
+}
